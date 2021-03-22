feaa08335caedfb7b53b9bcb9ff69b6d7bd72b8b
lokeshkumar9600 <lokesh960077152@gmail.com>
2021-03-22 11:30:17 +0530

    Registration module base files added

--- server.js
+++ server.js
@@ -4,8 +4,8 @@
 
 const express = require("express")
 const app = express();
-// const mongoose = require("mongoose"); 
-// mongoose.connect("mongodb://localhost:27017/Cargo" , {useNewUrlParser : true , useUnifiedTopology:true});
+const mongoose = require("mongoose"); 
+mongoose.connect("mongodb://localhost:27017/Cargo" , {useNewUrlParser : true , useUnifiedTopology:true});
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
@@ -23,24 +23,36 @@ app.get("/sign-up",(req,res)=>{
 app.get("/sign-in" ,(req,res)=>{
     res.sendFile(__dirname+"/sign-in.html");
 })
+
+//REGISTRATION MODULE CODES
+
 app.post("/sign-up",(req,res)=>{
-    // generation of accounts and adds to customer collection in database 
-//    if(req.body.password == req.body.retypePassword){
-//        customers.create({
-//            firstname : req.body.firstname,
-//            lastname:req.body.lastname,
-//            email:req.body.email,
-//            password:req.body.password
-//        },(err,save)=>{
-//            if(err){
-//                console.log(err);
-//            }else{
-//                console.log(save);
-//                res.redirect("/")
-//            }
-//        })
-//    }
+// generation of accounts and adds to customer collection in database 
+// checks if the entered password and retyped password is correct or not if correct
+//the firstname,lastname,email,password is stored in the database
+
+   if(req.body.password == req.body.retypePassword){
+       customers.create({
+           firstname : req.body.firstname,
+           lastname:req.body.lastname,
+           email:req.body.email,
+           password:req.body.password
+       },(err,save)=>{       // callback function to check the error and success details 
+           if(err){
+               console.log(err);
+           }else{
+               console.log(save);
+               res.redirect("/")
+           }
+       });
+   }else   // if entered password and redirected password is not correct then the website redirects to the same page
+   {  
+       res.redirect("/sign-up");
+   } 
+
 });
+
+
 app.listen(3000,(req,res)=>{
     console.log("server is running");
     
@@ -49,28 +61,30 @@ app.listen(3000,(req,res)=>{
 
 
 //database section (only the schemas initialisation)
-// var orderSchema = new mongoose.Schema({
-//     pro_name : String,
-//     weight: Number,
-//     pro_cost:Number,
-//     Made_in:String,
-//     ship_amt : Number, //will be set default for all orders 
-//     address: String,
-//     transactions_det:[transactions]
-// });
-// var transactions = new mongoose.Schema({
-//     cardnum : String,
-//     exp_date: String,
-//     cv_code : Number,
-//     card_owner: String
-// });
-// var customerSchema = new mongoose.Schema({
-//     firstname : String,
-//     lastname: String,
-//     email : String,
-//     password: String,
-//     orders:[]
-// });
-// var customers = mongoose.model("customer",customerSchema);
+var orderSchema = new mongoose.Schema({
+    pro_name : String,
+    weight: Number,
+    pro_cost:Number,
+    Made_in:String,
+    ship_amt : Number, //will be set default for all orders 
+    address: String,
+    transactions_det:[]
+});
+var transactions = new mongoose.Schema({
+    cardnum : String,
+    exp_date: String,
+    cv_code : Number,
+    card_owner: String
+});
+
+//REGISTRATION MODULE SCHEMA 
+var customerSchema = new mongoose.Schema({
+    firstname : String,
+    lastname: String,
+    email : String,
+    password: String,
+    orders:[]
+});
+var customers = mongoose.model("customer",customerSchema);
 
 //end of the db schema initialisation section 
